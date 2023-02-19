adc502d98c9b2c26255a8d1f962b3e8752ad9c74
Pavel Chekalin <grokki91@gmail.com>
2023-02-19 22:53:44 +0300

    Add destructuring props

--- function_component/src/ShopItemFunc.js
+++ function_component/src/ShopItemFunc.js
@@ -1,15 +1,15 @@
 export default function ShopItemFunc(props) {
+    const { brand, title, description, descriptionFull, currency, price } = props.item;
     return (
         <div className='main-content'>
-            <h2>{props.item.brand}</h2>
-            <h1>{props.item.title}</h1>
-            <h3>{props.item.description}</h3>
-            <div className='description'>{props.item.descriptionFull}</div>
-            <div className='highlight-window mobile'>
-                <div className='highlight-overlay'></div>
-            </div>
+            <h2>{brand}</h2>
+            <h1>{title}</h1>
+            <h3>{description}</h3>
+            <div className='description'>{descriptionFull}</div>
+            <div className='highlight-window mobile'><div className='highlight-overlay'></div></div>
+            <div className='divider'></div>
             <div className='purchase-info'>
-                <div className='price'>{props.item.currency}{props.item.price}</div>
+                <div className='price'>{currency}{price.toFixed(2)}</div>
                 <button>Добавить в корзину</button>
             </div>
         </div>
